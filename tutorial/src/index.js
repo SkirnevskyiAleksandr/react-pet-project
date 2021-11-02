c5076f259bac22432f64f33329cef8deb86d2e41
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-11-02 20:31:20 +0200

    feat:book

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Greeting(){
-  return <h4>sdfsfs</h4>
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Title />
+      <Author />
+    </section>
+  )
+};
+const Book = () => {
+  return <article>
+    <Image />
+  </article>
 }
+const Image = () => {
+  return (<img src="https://images-na.ssl-images-amazon.com/images/I/91JV0+3BRGL._AC_UL254_SR254,254_.jpg" alt="Dune" />)
+}
+const Title = () => <h1>0.00 Free with Audible trial</h1>
+const Author = () => <h4>Alex</h4>
 
-ReactDom.render(<Greeting/>, document.getElementById('root'))
+ReactDom.render(<BookList />, document.getElementById('root'))
