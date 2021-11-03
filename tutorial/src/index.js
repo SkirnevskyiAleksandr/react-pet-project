a089fb51d088cec148fb631e11240ff293d1fc13
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-11-03 18:56:19 +0200

    feat:chenge-book

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -14,15 +14,14 @@ function BookList() {
 };
 const Book = () => {
   return <article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <Image style />
+    <h1>0.00 Free with Audible trial</h1>
+    <h4>Alex</h4>
   </article>
 }
 const Image = () => {
   return (<img src="https://images-na.ssl-images-amazon.com/images/I/91JV0+3BRGL._AC_UL254_SR254,254_.jpg" alt="Dune" />)
 }
-const Title = () => <h1>0.00 Free with Audible trial</h1>
-const Author = () => <h4>Alex</h4>
+
 
 ReactDom.render(<BookList />, document.getElementById('root'))
