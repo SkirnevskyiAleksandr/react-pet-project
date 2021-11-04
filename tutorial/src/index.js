2827c1a480308b3e90a8a0c142041a49e5509e86
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-11-04 22:28:32 +0200

    feat:seconBook

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,25 +2,61 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css';
 
+
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91JV0+3BRGL._AC_UL254_SR254,254_.jpg',
+  title: "random",
+  number: 22,
+  author: "Alex"
+};
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81z6xJd6mrS._AC_UL254_SR254,254_.jpg',
+  title: "Big Shot Diary of a Wimpy Kid",
+  number: 662,
+  author: "Pahan"
+};
+
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+        number={firstBook.number}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+        number={secondBook.number}
+      />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+        number={firstBook.number}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+        number={secondBook.number}
+      />
     </section>
   )
 };
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
   return <article className="book">
-    <Image style />
-    <h1>0.00 Free with Audible trial</h1>
-    <h4>Alex</h4>
+    <img src={props.img} alt="dune" />
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
+    <h4>{props.number}</h4>
+
   </article>
-}
-const Image = () => {
-  return (<img src="https://images-na.ssl-images-amazon.com/images/I/91JV0+3BRGL._AC_UL254_SR254,254_.jpg" alt="Dune" />)
 }
 
 
