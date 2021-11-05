5f725c9a5b248deb7b656d1e6667fc85a0355692
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-11-06 00:02:00 +0200

    feat

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,67 +2,94 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css';
 
+const Books = [
+    {
+        img: 'https://images-na.ssl-images-amazon.com/images/I/91JV0+3BRGL._AC_UL254_SR254,254_.jpg',
+        title: "random",
+        number: 22,
+        author: "Alex"
+    }
+    ,
+    {
+        img: 'https://images-na.ssl-images-amazon.com/images/I/81z6xJd6mrS._AC_UL254_SR254,254_.jpg',
+        title: "Big Shot Diary of a Wimpy Kid",
+        number: 662,
+        author: "Pahan"
+    }
+]
 
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/91JV0+3BRGL._AC_UL254_SR254,254_.jpg',
-  title: "random",
-  number: 22,
-  author: "Alex"
-};
-
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81z6xJd6mrS._AC_UL254_SR254,254_.jpg',
-  title: "Big Shot Diary of a Wimpy Kid",
-  number: 662,
-  author: "Pahan"
-};
-
-
-function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-        number={firstBook.number}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-        number={secondBook.number}
-      />
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-        number={firstBook.number}
-      >
-        <p>Loren10</p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-        number={secondBook.number}
-      />
-    </section>
-  )
-};
-const Book = (props) => {
-  // const { img, title, author, number } = props;
-  console.log(props)
-  return <article className="book">
-    <img src={props.img} alt="dune" />
-    <h1>{props.title}</h1>
-    <h4>{props.author}</h4>
-    <h4>{props.number}</h4>
-    <h3>{props.children}</h3>
-
-  </article>
+function BookList(books) {
+    return (
+        <section>
+            {
+                Books.map((obj) => {
+                    return (
+                        <Book
+                            nam={obj}
+                        />
+                    )
+                })
+            }
+        </section>
+    )
 }
 
+function Book(props) {
+    return (
+        <section>
+            <img src={props.nam.img} alt="" />
+            <h1>{props.nam.title}</h1>
+            <h2>{props.nam.author}</h2>
+        </section>
+    )
+}
 
-ReactDom.render(<BookList />, document.getElementById('root'))
+// function BookList() {
+//     return (
+//         <section className="booklist">
+//             {
+//                 Books.map((book) => {
+//                     return (
+//                         <Book
+//                             name={book}
+//                         />
+//                     )
+//                 })
+//             }
+//         </section>
+//     )
+// };
+// const Book = (props) => {
+//     return <article className="book">
+//         <img src={props.name.img} alt="dune" />
+//         <h1>{props.name.title}</h1>
+//         <h4>{props.name.author}</h4>
+//         <h4>{props.name.number}</h4>
+
+//     </article>
+// }
+
+
+
+
+ReactDom.render(<BookList />, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
